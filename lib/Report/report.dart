c47e9ebce9b868e7b2eb4ed1fc2620deec681d0f
Piyush <piyushp761@users.noreply.github.com>
2019-09-11 20:25:18 +0530

    Fixed Report Form.

--- lib/Report/report.dart
+++ lib/Report/report.dart
@@ -7,35 +7,103 @@ class Report extends StatelessWidget {
       body: ReportForm(),
     );
   }
-  
 }
 
-class ReportForm extends StatelessWidget {
+class ReportForm extends StatefulWidget{
+  @override 
+  _ReportForm createState() => new _ReportForm();
+}
+
+class _ReportForm extends State<ReportForm> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        const SizedBox(height: 24.0),
-        const SizedBox(height: 24.0),
-        TextFormField(
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: 'Tell us what is wrong and how it can be fixed',
-            helperText: 'Keep it short, and to the point.',
-            labelText: 'Describe the Problem',
-          ),
-          maxLines: 3,
-        ),
-        new DropdownButton<String>(
-          items: <String>['A', 'B', 'C', 'D'].map((String value) {
-            return new DropdownMenuItem<String>(
-              value: value,
-              child: new Text(value),
-            );
-          }).toList(),
-          onChanged: (_) {},
-        )
-      ],
-    );
+    return Padding(
+        padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+        child: Column(
+          
+          children: <Widget>[
+            const SizedBox(height: 24.0),
+            const SizedBox(height: 24.0),
+            TextFormField(
+              scrollPadding: EdgeInsets.all(10.0),
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Tell us what is wrong and how it can be fixed',
+                helperText: 'Keep it short, and to the point.',
+                labelText: 'Describe the Problem',
+              ),
+              maxLines: 3,
+            ),
+            DropDownButtonPage(),
+            
+          ],
+        ));
   }
 }
+
+class DropDownButtonPage extends StatefulWidget {
+  @override
+  _DropDownButtonState createState() => _DropDownButtonState();
+}
+
+class _DropDownButtonState extends State<DropDownButtonPage> {
+  String _value;
+
+
+  DropdownButton _itemDown() => DropdownButton<String>(
+        hint: Text(
+          "Please select the type of problem!",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        items: [
+          DropdownMenuItem(
+            value: "1",
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(Icons.build),
+                SizedBox(width: 10),
+                Text(
+                  "Problem Type 1",
+                ),
+              ],
+            ),
+          ),
+          DropdownMenuItem(
+            value: "2",
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(Icons.settings),
+                SizedBox(width: 10),
+                Text(
+                  "Problem Type 2",
+                ),
+              ],
+            ),
+          ),
+        ],
+        onChanged: (value) {
+          setState(() {
+            _value = value;
+          });
+        },
+        value: _value,
+        isExpanded: true,
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+        child: Column(
+          children: <Widget>[
+            _itemDown(),
+          ],
+        ),
+      );
+  }
+}
+
